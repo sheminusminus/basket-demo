112adb99824cb357267ba475f5c1d8997b925151
Emily Kolar <emkolardev@gmail.com>
2018-01-28 17:35:22 -0600

    add db functions to wrapped component

--- client/app/wrappers/withFirebaseDb.js
+++ client/app/wrappers/withFirebaseDb.js
@@ -1,12 +1,56 @@
 import React from 'react';
 
-export default function withFirebaseData(Component, db) {
-  return class extends React.Component {
+export default function withFirebaseDb(Component, db) {
+  class ComponentWithDb extends React.Component {
+    static handleAddItem(name, quantity = 1, recurring = false) {
+      const { key } = db.ref('items').push();
+
+      db.ref(`items/${key}`).set({
+        name,
+        quantity,
+        recurring,
+      });
+      db.ref(`list/${key}`).set(true);
+    }
+
+    static handleRemoveItem(itemId) {
+      db.ref(`items/${itemId}`).set(null);
+      db.ref(`list/${itemId}`).set(null);
+    }
+
+    static handleAddItemToBasket(itemId) {
+      db.ref('basket').update({ [itemId]: true });
+      db.ref(`list/${itemId}`).update(null);
+    }
+
+    static handleRemoveItemFromBasket(itemId) {
+      db.ref('basket').update({ [itemId]: null });
+      db.ref(`list/${itemId}`).update(true);
+    }
+
+    static handleAddItemToList(itemId) {
+      db.ref('list').update({ [itemId]: true });
+    }
+
+    static handleRemoveItemFromList(itemId) {
+      db.ref('list').update({ [itemId]: null });
+    }
+
     render() {
       const { ...props } = this.props;
       return (
-        <Component db={db} { ...props } />
+        <Component
+          db={db}
+          handleAddItemToBasket={ComponentWithDb.handleAddItemToBasket}
+          handleRemoveItemFromBasket={ComponentWithDb.handleRemoveItemFromBasket}
+          handleRemoveItem={ComponentWithDb.handleRemoveItem}
+          handleAddItem={ComponentWithDb.handleAddItem}
+          handleAddItemToList={ComponentWithDb.handleAddItemToList}
+          handleRemoveItemFromList={ComponentWithDb.handleRemoveItemFromList}
+          { ...props } />
       );
     }
-  };
+  }
+
+  return ComponentWithDb;
 }
