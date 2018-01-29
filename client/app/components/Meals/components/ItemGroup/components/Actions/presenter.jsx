955b98999dd2b24edec02bc019aee243bab6c4b9
Emily Kolar <emkolardev@gmail.com>
2018-01-28 21:38:25 -0600

    meal section styles (maybe)

--- client/app/components/Meals/components/ItemGroup/components/Actions/presenter.jsx
+++ client/app/components/Meals/components/ItemGroup/components/Actions/presenter.jsx
@@ -3,7 +3,5 @@ import React from 'react';
 import styles from './styles.scss';
 
 export default ({ handleAddItemsToList }) => (
-  <button className={styles.actions} onClick={handleAddItemsToList}>
-    Add to Shopping List
-  </button>
+  <button className={styles.actions} onClick={handleAddItemsToList} />
 );
--- client/app/components/Meals/components/ItemGroup/presenter.jsx
+++ client/app/components/Meals/components/ItemGroup/presenter.jsx
@@ -6,7 +6,9 @@ import styles from './styles.scss';
 
 export default ({ groupKey, name = '', items = [], handleAddItemsToList }) => (
   <div className={styles.groupWrapper}>
-    <h5>{name}</h5><Actions handleAddItemsToList={handleAddItemsToList} />
+    <div className={styles.groupHeader}>
+      <h5>{name}</h5><Actions handleAddItemsToList={handleAddItemsToList} />
+    </div>
     <ul>
       {items.map(item => <MealItem key={`${groupKey}-${item.id}`} item={item} />)}
     </ul>
