3fea9dde9bb9d352ff8eaca6132289a5fc2fb424
Emily Kolar <emkolardev@gmail.com>
2018-01-28 18:14:11 -0600

    add listeners/getters

--- client/app/wrappers/withFirebaseDb.js
+++ client/app/wrappers/withFirebaseDb.js
@@ -10,6 +10,7 @@ export default function withFirebaseDb(Component, db) {
         quantity,
         recurring,
       });
+
       db.ref(`list/${key}`).set(true);
     }
 
@@ -36,11 +37,41 @@ export default function withFirebaseDb(Component, db) {
       db.ref('list').update({ [itemId]: null });
     }
 
+    static listenToItems(callback) {
+      db.ref('items').on('value', snapshot => callback(snapshot.val()));
+    }
+
+    static getItemsOnce(callback) {
+      db.ref('items').once('value', snapshot => callback(snapshot.val()));
+    }
+
+    static listenToBasket(callback) {
+      db.ref('basket').on('value', snapshot => callback(snapshot.val()));
+    }
+
+    static getBasketOnce(callback) {
+      db.ref('basket').once('value', snapshot => callback(snapshot.val()));
+    }
+
+    static listenToMeals(callback) {
+      db.ref('meals').on('value', snapshot => callback(snapshot.val()));
+    }
+
+    static getMealsOnce(callback) {
+      db.ref('meals').once('value', snapshot => callback(snapshot.val()));
+    }
+
     render() {
       const { ...props } = this.props;
       return (
         <Component
           db={db}
+          listenToItems={ComponentWithDb.listenToItems}
+          listenToBasket={ComponentWithDb.listenToBasket}
+          listenToMeals={ComponentWithDb.listenToMeals}
+          getItemsOnce={ComponentWithDb.listenToItems}
+          getBasketOnce={ComponentWithDb.getBasketOnce}
+          getMealsOnce={ComponentWithDb.getMealsOnce}
           handleAddItemToBasket={ComponentWithDb.handleAddItemToBasket}
           handleRemoveItemFromBasket={ComponentWithDb.handleRemoveItemFromBasket}
           handleRemoveItem={ComponentWithDb.handleRemoveItem}
